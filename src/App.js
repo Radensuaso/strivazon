0471bf0a805d93d84036c8d48e74e0803abb301d
Radensuaso <andrels9283@gmail.com>
2021-08-27 10:50:44 +0100

    Navbar done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./views/Home";
+import StrivazonNavbar from "./components/NavbarFooter/StrivazonNavbar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Router>
+          <StrivazonNavbar />
+          <Route path="/" exact component={Home} />
+        </Router>
       </div>
     );
   }
---
+++ src/views/Home.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import { Container } from "react-bootstrap";
+
+class Home extends Component {
+  render() {
+    return <Container></Container>;
+  }
+}
+
+export default Home;
